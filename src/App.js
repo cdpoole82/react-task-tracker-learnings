01b076f6774114bc7dca22d0e7ec9b870adefa73
cdpoole <poole.christopher@gmail.com>
2021-08-12 00:19:08 -0500

    Added JSON Server and db file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //imports here
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
@@ -10,49 +10,81 @@ import AddTask from './components/AddTask'
 function App() {
   //current task datastore 'state'
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Walk Clark (the dog)',
-      day: 'July 9th at 20:00',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Walk Huey (the dog)',
-      day: 'July 9th at 20:15',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Walk Bros (the humans)',
-      day: 'July 9th at 20:30',
-      reminder: false,
-    },
-    {
-      id: 4,
-      text: 'Consume Inbound (the beer)',
-      day: 'July 9th at 20:45',
-      reminder: false,
-    },
-  ])
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+     
+    const getTasks = async () => {
+       const tasksFromServer = await fetchTasks()
+       setTasks(tasksFromServer)
+     } 
+     
+     getTasks()
+
+  }, []) //empty array could be used to pass in a value like a username or other variable for the request
+
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    
+    return data    
+  }
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    
+    return data    
+  }
+    
 
   //Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
+  const addTask = async (task) => {
+    
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+    
+    /* const id = Math.floor(Math.random() * 10000) + 1
     const newTask = {id, ...task}
-    setTasks([...tasks, newTask])
+    setTasks([...tasks, newTask]) */
   }
 
   //Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+      await fetch(`http://localhost:5000/tasks/${id}`, {
+        method: 'DELETE',
+      })    
+    
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle, 
+    reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {      
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(updTask)  
+  })  
+   
+  const data = await res.json()
+
     setTasks(tasks.map((task) => task.id === id
-      ? { ...task, reminder: !task.reminder } : task
+      ? { ...task, reminder: data.reminder } : task
       )
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);//this tells React to take our App.js and render it inside the Div=Root tag on the base index.html page
-//App.js is the 'Root Component' of the structure
+);
 
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
